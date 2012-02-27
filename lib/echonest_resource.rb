a18b318d16df40217ac5de88360df9a481430043
tzumby <razvan@wisecatmedia.com>
2012-02-27 16:35:03 -0500

    Loading the Echonest API key from the config/echonest.yml file

--- lib/echonest_resource.rb
+++ lib/echonest_resource.rb
@@ -1,5 +1,19 @@
 require "echonest_resource/version"
+require 'active_support'
+require 'httparty'
+require 'hashie'
+
+class APIKeyMissing     < StandardError; end
+class MissingConfigFile < StandardError; end
 
 module EchonestResource
-  # Your code goes here...
+  def self.api_key
+    echonest_config = YAML.load_file("#{Rails.root}/config/echonest.yml")
+    raise MissingConfigFile if echonest_config.blank?
+    echonest_config["api_key"]
+  end
 end
+
+directory = File.expand_path(File.dirname(__FILE__))
+
+require File.join(directory, 'echonest_resource', 'base')
